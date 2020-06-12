bf37dedf0dcbd5ef28d8ceef4f2b2619bb509cc1
Marco Estrella <mestrella.1991@gmail.com>
2020-06-12 23:25:29 +1000

    Worked on commit script

--- commit.sh
+++ commit.sh
@@ -2,47 +2,54 @@
 
 COMMENT=$1
 
-echo "$COMMENT"
+PROJECT_DIR=$(pwd)
+SITE_DIR="$PROJECT_DIR/_site"
 
 #   Build /_site
-bundle exec jekyll clean
+
+if [ -d "$SITE_DIR" ]; then
+   bundle exec jekyll clean
+fi
+
 bundle exec jekyll build
 
-#Commit jekyll files to develop
+#   Commit jekyll files to develop
 git add --all
 git commit -m "$COMMENT"
 git push
 
-#   Caches project path and Creates a backup folder for _site
-
-PROJECT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-PROJECT_DIR=$(pwd)
-SITE_DIR="$PROJECT_DIR/_site"
+#   Creates a backup folder for _site
 BACKUP_DIR_NAME="backup_site"
 
 cd ..
+PARENT_DIR=$(pwd)
+BACKUP_DIR="$PARENT_DIR/$BACKUP_DIR_NAME"
+
+if [ -d "$BACKUP_DIR" ]; then
+   rmdir $BACKUP_DIR
+fi
+
 mkdir $BACKUP_DIR_NAME
 cd $BACKUP_DIR_NAME
-BACKUP_DIR=$(pwd)
-echo $BACKUP_DIR
 
 #   Copy ~/_site in ~/backup_site
 cp -a "$SITE_DIR/." $BACKUP_DIR
 
-#Change to master master branch
+#   Change to master master branch
 cd $PROJECT_DIR
-#git checkout master
 
-#remove and move the files from /backup_site
-#rm *
-#cp -f -a "$BACKUP_DIR/." $PROJECT_DIR
+git checkout master
 
-#Commit HTML to master
-#git add --all
-#git commit -m "$COMMENT"
-#git push
+#   remove and copy the files from /backup_site to the project
+rm *
+cp -a "$BACKUP_DIR/." $PROJECT_DIR
 
-#Clean everything
+#   Commit HTML to master
+git add .
+git commit -m "$COMMENT"
+git push
+
+#   Clean everything
 #git checkout develop
-#rm $BACKUP_DIR
-#bundle exec jekyll build
+rmdir $BACKUP_DIR
+#bundle exec jekyll build 
